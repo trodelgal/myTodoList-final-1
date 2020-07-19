a73e893db5304b7673338baafc9b7b91f496f09b
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-07-19 22:27:50 +0300

    all Except from sort

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const prioritySelector = document.querySelector('#prioritySelector');
 const toDoTextInput = document.querySelector('#textInput');
 const viewSection = document.querySelector('.view');
 const sortButton = document.querySelector('#sortButton');
-
 let taskList = [];
+
 
 //PROPERTIES OF OBJECT
 const getPriority = () => {
@@ -38,58 +38,54 @@ function creatTask() {
 //GET THE OBJECT TO ARRAY AND PRINT THE OBJECT TO HTML with counter
 function putTaskIntoTaskArray() {
     taskList.push(creatTask());
-    console.log(taskList);
-    viewSection.innerHTML = '';
-    showTask(taskList);
+    viewSection.appendChild(createToDoListHTML())
     countTheList(taskList);
+    toDoTextInput.value = '';
 }
 
 //CREATE THE LIST IN THE CONTAINER DIV
-function createToDoListHTML(task) {
+function createToDoListHTML() {
     const taskContainerDiv = document.createElement('div');
     taskContainerDiv.setAttribute('class', 'todoContainer');
 
     const taskrPiorityDiv = document.createElement('div');
-    taskrPiorityDiv.setAttribute('class', 'priority');
-    taskrPiorityDiv.innerHTML = task.priority;
+    taskrPiorityDiv.setAttribute('class', 'todoPriority');
+    taskrPiorityDiv.textContent = prioritySelector.value;
 
     const taskTodoCreatedAtDiv = document.createElement('div');
     taskTodoCreatedAtDiv.setAttribute('class', 'todoCreatedAt');
-    taskTodoCreatedAtDiv.innerHTML = task.todoCreatedAt;
+    const today = new Date();
+    let zero = today.getMonth() < 10 ? '0' : '';
+    let zeroSeconds = today.getSeconds() < 10 ? '0' : '';
+    const createAt = `${today.getDate()}-${zero}${today.getMonth() + 1}-${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${zeroSeconds}${today.getSeconds()}`;
+    taskTodoCreatedAtDiv.textContent = createAt;
     
     const taskTodoTextDiv = document.createElement('div');
     taskTodoTextDiv.setAttribute('class', 'todoText');
-    taskTodoTextDiv.innerHTML = task.todoText;
+    taskTodoTextDiv.textContent = toDoTextInput.value;
 
-    const deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
     deleteButton.textContent = "delete";
     deleteButton.setAttribute('class', 'deleteButton');
     
-    taskContainerDiv.append(taskrPiorityDiv, taskTodoCreatedAtDiv, taskTodoTextDiv, deleteButton);
+    taskContainerDiv.appendChild(taskrPiorityDiv);
+    taskContainerDiv.appendChild(taskTodoCreatedAtDiv);
+    taskContainerDiv.appendChild(taskTodoTextDiv);
     
     return taskContainerDiv;
 }
 
 //ADD THE CONTAINER DIV TO THE VIEW SECTION
-function addTaskContainerDivToTheView(taskContainerDiv){
-    viewSection.appendChild(taskContainerDiv);
-}
-addButton.addEventListener('click', putTaskIntoTaskArray)
-
-//PRINT THE OBJECT TO HTML
-function showTask(taskList) {
-    toDoTextInput.value = '';
-    viewSection.innerHTML = '';
-    for (const task of taskList) {
-    addTaskContainerDivToTheView(createToDoListHTML(task));
-    }
-}
+// function addTaskContainerDivToTheView(taskContainerDiv){
+//     viewSection.appendChild(taskContainerDiv);
+// }
+addButton.addEventListener('click', putTaskIntoTaskArray);
 
 //SORT THE LIST BY THE PRIORITY
 function sortListByPriority() {
     let taskList1 = taskList;
     console.log(taskList1);
-    taskListSorted = taskList1.sort((a, b) => (a.priority < b.priority) ? 1 : -1);
+    taskListSorted = taskList1.sort((a, b) => (a.priority > b.priority) ? 1 : -1);
     showTask(taskListSorted);
 }
 sortButton.addEventListener('click', sortListByPriority)
@@ -98,11 +94,7 @@ sortButton.addEventListener('click', sortListByPriority)
 function countTheList(){
     const counter = document.querySelector('#counter');
     let x = taskList.length;
-    counter.innerHTML = `Things to do: ${x}`;
+    counter.innerHTML = x;
     return counter
 }
 
-function toDelete(event){
-    viewSection.removeChild(this.taskContainerDiv);
-}
-deleteButton.addEventListener('click', toDelete);
